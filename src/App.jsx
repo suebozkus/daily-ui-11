c67935ff8f97e5da19652bb75e2a1ac08e85a5ec
suebozkus <75066493+suebozkus@users.noreply.github.com>
2022-09-14 12:55:46 +0200

    Turnery operator testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import logo from "./logo.svg";
+
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [ismodalopen, setismodalopen] = useState(false);
+  const [issuccess, setissuccess] = useState(false);
   return (
     <div className="App">
       <h1>Trigger message</h1>
-      <button>Message</button>
-      <div className="modal d-none">
-        <h2>Successbox</h2>
-        <p>This was successful.</p>
-        <button>Confirm</button>
+      <button
+        onClick={(e) => {
+          setismodalopen(!ismodalopen);
+        }}
+      >
+        Open modal
+      </button>
+
+      <button
+        onClick={(e) => {
+          setissuccess(!issuccess);
+        }}
+      >
+        Change to success
+      </button>
+      {!issuccess ? <h1>Red</h1> : <h1>Green</h1>}
+
+      <div
+        className={`modal ${ismodalopen ? "" : "d-none"} ${
+          issuccess ? "success" : "danger"
+        }`}
+      >
+        <h2>{issuccess ? "Successbox" : "Dangerbox"}</h2>
+        <p>{issuccess ? "This was successful." : "This was not successful."}</p>
+        <button
+          onClick={(e) => {
+            setismodalopen(false);
+          }}
+        >
+          Confirm
+        </button>
       </div>
     </div>
   );
